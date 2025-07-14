078ff8c0693fec2efbbb3f06136973b467d3c18b
DakshYdv <daksh.ydv2411@gmail.com>
2025-07-14 21:07:07 +0530

    upstack-livelink-init

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -25,6 +25,7 @@ export const projects: ProjectTypes[] = [
     timeline: "March 2024 - April 2025",
     src: "/upstack2.png",
     gitUrl: "https://github.com/dakshydv/UpStack.git",
+    webUrl: "https://up-stack.vercel.app/",
     skills: ["turborepo", "nextjs", "reactjs", "tailwindcss", "expressjs", "nodemailer", "redis streams"],
     desc: "Stop guessing if your APIs are down. Upstack quietly watches your endpoints and alerts you before users notice failures. Get cron-like monitoring without the cron headaches.",
   },
