2d0beee42103e2bc724315dccfa99abd72c586e5
DakshYdv <daksh.ydv2411@gmail.com>
2025-08-06 23:15:20 +0530

    infinidraw added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,7 +12,7 @@ export interface ExperienceTypes {
   company: string;
   timeline: string;
   role: string;
-  desc: string
+  desc: string;
 }
 
 export function cn(...classes: (string | undefined | null | false)[]): string {
@@ -21,12 +21,33 @@ export function cn(...classes: (string | undefined | null | false)[]): string {
 
 export const projects: ProjectTypes[] = [
   {
-    title: "UpStack (Coming Soon)",
+    title: "Infinidraw (In Progress)",
+    timeline: "July 2025 - Present",
+    src: "/infinidraw.png",
+    gitUrl: "https://github.com/dakshydv/infinidraw.git",
+    webUrl: "https://infinidraw-mocha.vercel.app/canvas/1/1",
+    skills: [
+      "nextjs",
+      "reactjs",
+      "tailwindcss",
+      "websockets"
+    ],
+    desc: "A minimalist, collaborative whiteboard tool built with React and TypeScript, featuring real-time drawing, shapes, and text. Supports exporting sketches and offline functionality. Designed for simplicity and ease of use, inspired by Excalidraw.",
+  },
+  {
+    title: "UpStack",
     timeline: "March 2024 - April 2025",
     src: "/upstack2.png",
     gitUrl: "https://github.com/dakshydv/UpStack.git",
     webUrl: "https://up-stack.vercel.app/",
-    skills: ["nextjs", "reactjs", "tailwindcss", "expressjs", "nodemailer", "redis streams"],
+    skills: [
+      "nextjs",
+      "reactjs",
+      "tailwindcss",
+      "expressjs",
+      "nodemailer",
+      "redis streams",
+    ],
     desc: "Stop guessing if your APIs are down. Upstack quietly watches your endpoints and alerts you before users notice failures. Get cron-like monitoring without the cron headaches.",
   },
   {
@@ -43,7 +64,14 @@ export const projects: ProjectTypes[] = [
     timeline: "Dec 2024 - Jan 2025",
     src: "/blinkchat.png",
     gitUrl: "https://github.com/dakshydv/BlinkChat.git",
-    skills: ["reactjs", "expressjs", "tailwindcss","postgreSQL", "websockets", "jsonwebtoken"],
+    skills: [
+      "reactjs",
+      "expressjs",
+      "tailwindcss",
+      "postgreSQL",
+      "websockets",
+      "jsonwebtoken",
+    ],
     desc: "A lightweight chat app where messages appear instantly. No lag, no refreshes – just smooth conversations that stay synced across devices.",
   },
   {
@@ -64,23 +92,23 @@ export const projects: ProjectTypes[] = [
   },
 ];
 
-export const Experiences:ExperienceTypes[]  = [
+export const Experiences: ExperienceTypes[] = [
   {
     company: "Freelancer",
     timeline: "December 2024 - Present",
     role: "Full Stack Developer",
-    desc: "Designed and developed custom websites for clients, including a fully functional customs garage site with booking forms, automated email services, and responsive UI/UX. Collaborated directly with clients to gather requirements, implement solutions, and ensure timely project delivery. Specialized in front-end development, performance optimization, and seamless third-party integrations to enhance user experience and business efficiency."
+    desc: "Designed and developed custom websites for clients, including a fully functional customs garage site with booking forms, automated email services, and responsive UI/UX. Collaborated directly with clients to gather requirements, implement solutions, and ensure timely project delivery. Specialized in front-end development, performance optimization, and seamless third-party integrations to enhance user experience and business efficiency.",
   },
   {
     company: "Midnight Customs, Bolton, Canada (Remote)",
     timeline: "March 2025 - May 2025",
     role: "Full Stack Developer",
-    desc: "Designed and developed a responsive customs garage website with dynamic booking forms and integrated email services (SMTP/API) for automated appointment confirmations. Built an intuitive UI/UX for seamless customer interaction while ensuring cross-browser compatibility and mobile optimization. Collaborated with stakeholders to deliver a fully functional site, improving engagement and operational efficiency."
+    desc: "Designed and developed a responsive customs garage website with dynamic booking forms and integrated email services (SMTP/API) for automated appointment confirmations. Built an intuitive UI/UX for seamless customer interaction while ensuring cross-browser compatibility and mobile optimization. Collaborated with stakeholders to deliver a fully functional site, improving engagement and operational efficiency.",
   },
   {
     company: "Coding Club, BITS Pilani",
     timeline: "August 2024 - Present",
     role: "Member",
-    desc: "Actively contributed to the college coding club by participating in hackathons, workshops, and collaborative projects to enhance technical skills in web development and problem-solving. Engaged in peer programming, code reviews, and knowledge-sharing sessions to foster a learning-driven community. Assisted in organizing tech events and coding competitions, gaining exposure to emerging technologies and teamwork in software development."
-  }
-]
+    desc: "Actively contributed to the college coding club by participating in hackathons, workshops, and collaborative projects to enhance technical skills in web development and problem-solving. Engaged in peer programming, code reviews, and knowledge-sharing sessions to foster a learning-driven community. Assisted in organizing tech events and coding competitions, gaining exposure to emerging technologies and teamwork in software development.",
+  },
+];
