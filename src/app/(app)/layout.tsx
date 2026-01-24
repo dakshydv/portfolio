b36e2d3a4de87718d442e2be4f2f53b6b797c2af
dakshydv <dakshyadav.dev@gmail.com>
2026-01-24 16:35:45 +0530

    remove footer

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -12,7 +12,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     <>
       <SiteHeader />
       <main className="max-w-screen overflow-x-hidden px-2">{children}</main>
-      <SiteFooter />
+      {/* <SiteFooter /> */}
       <ScrollToTop />
     </>
   );
